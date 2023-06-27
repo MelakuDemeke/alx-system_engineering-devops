a95ae895c31cd86d48f472d0705b8037e208acc9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-27 14:26:17 +0300

    install nginx and write to file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,14 @@
 # setup new ubunu server with nginx using puppet
 
 exec { 'update system':
-command => 'apt-get update',
+    command => 'apt-get update',
 }
 
+package { 'nginx':
+    ensure => 'installed',
+    require => Exec['update system'],
+}
+
+file { '/var/www/html/index.html':
+    content => 'Hello World',
+}
