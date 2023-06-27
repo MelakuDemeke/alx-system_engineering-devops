0727f688ceb13181eee75c38663c1a7ba62c27d0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-06-27 14:36:07 +0300

    update code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,24 @@
 # setup new ubunu server with nginx using puppet
 
 exec { 'update system':
-    command => 'apt-get update',
+    command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-    ensure => 'installed',
-    require => Exec['update system'],
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-    content => 'Hello World',
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-exec { 'redirect_me':
-    command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell'
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
-    ensure => 'running',
-    require => Package['nginx'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
