8d1831027ef6bc35b26bc5289cd7ed45fff63c29
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-24 12:13:58 +0300

    ret to main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ Usage:
     python script_name.py employee_id
 
 Arguments:
-    employee_id (int): The ID of the employee whose to-do list you want to view
+    employee_id (int): The ID of the employee whose to-do list you want to view.
 
 Example:
     python script_name.py 3
@@ -20,16 +20,34 @@ import requests
 import sys
 
 
+def get_employee_todos(employee_id):
+    """
+    Retrieves the to-do list for the specified employee ID and
+    displays completed tasks.
+
+    Parameters:
+        employee_id (int): The ID of the employee whose to-do list
+        you want to view.
+    """
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base_url + "users/{}".format(employee_id)).json()
+    todos = requests.get(base_url + "todos",
+                         params={"userId": employee_id}).json()
+
+    completed_tasks = [todo.get("title")
+                       for todo in todos if todo.get("completed")]
+    total_tasks = len(todos)
+
+    print("Employee {} has completed {}/{} tasks:".format(user.get("name"),
+          len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task))
+
+
 if __name__ == "__main__":
-    userID = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userID)).json()
-    tod = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                       .format(userID)).json()
-    completed_tasks = []
-    for task in tod:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-        print("Employee {} is done with tasks({}/{}):".
-                  format(user.get('name'), len(completed_tasks), len(tod)))
-        print("\n".join("\t {}".format(task) for task in completed_tasks))
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py employee_id")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todos(employee_id)
