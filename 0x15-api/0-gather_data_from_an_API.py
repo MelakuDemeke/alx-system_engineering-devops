8630187a9a3284502d58a09bbe6d04ba30032406
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-24 16:44:57 +0300

    redo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,14 @@
 #!/usr/bin/python3
-"""
-Employee To-Do List
-
-This script fetches and displays the to-do list information for
-a given employee ID.It uses the JSONPlaceholder API
-(https://jsonplaceholder.typicode.com/)
-
-Usage:
-    python script_name.py employee_id
-
-Arguments:
-    employee_id (int): The ID of the employee whose to-do list
-    you want to view.
-
-Example:
-    python script_name.py 3
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == '__main__':
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(base_url + 'users/{}'.format(sys.argv[1])).json()
-    todos = requests.get(base_url + 'todos',
-                         params={'userId': sys.argv[1]}).json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed_tasks = [title.get("title") for title in todos if
-                       title.get('completed') is True]
-    print(completed_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          len(completed_tasks),
-                                                          len(todos)))
-    [print("\t {}".format(title)) for title in completed_tasks]
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
